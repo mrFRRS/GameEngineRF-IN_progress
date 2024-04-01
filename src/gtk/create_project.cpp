0588d962a8e6ac2574ecebb9c2b788994d8359e3
mrFRRS <correos.play.g@gmail.com>
2024-03-31 18:51:58 -0600

    creating ui and functionality in second page of the notebook

--- src/gtk/create_project.cpp
+++ src/gtk/create_project.cpp
@@ -1,17 +1,9 @@
 #include "engine.h"
-#include <iostream>
-#include <stdio.h>
-#include <iostream>
-#include <stdio.h>
-//header import 
 #include "create_project.h"
-#include "filesystem"
-#include "fstream"
-#include "sys/stat.h"
+#include <iostream>
+#include <gtkmm.h>
+#include <filesystem>
 #include "dirent.h"
-#include "gtkmm.h"
-#include <unistd.h>
-
 
 //inicializate
 create_project::create_project()
@@ -19,13 +11,15 @@ create_project::create_project()
 //box
 //main_box
 vbox1(Gtk::Orientation::VERTICAL),
+
 //page_one
 vbox2_main(Gtk::Orientation::HORIZONTAL),
-// vbox2_image(Gtk::Orientation::VERTICAL),
 vbox2_UX(Gtk::Orientation::VERTICAL,10),
 vbox2_buttons(Gtk::Orientation::HORIZONTAL, 10),
+
 //page_two
-vbox3(Gtk::Orientation::HORIZONTAL, 10),
+vbox3_main(Gtk::Orientation::VERTICAL, 10),
+vbox3_scroll(Gtk::Orientation::VERTICAL),
 //page_three
 vbox4(Gtk::Orientation::VERTICAL),
 //labels
@@ -67,13 +61,12 @@ image("assets/images/violet.jpg")
   notebook.get_style_context()->add_provider(provider, GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
   notebook.get_style_context()->add_class("vbox-main");
   notebook.append_page(vbox2_main, "create");
-  notebook.append_page(vbox3, "open");
+  notebook.append_page(vbox3_main, "open");
   notebook.append_page(vbox4, "delete");
 
+  notebook.signal_switch_page().connect(sigc::mem_fun(*this, &create_project::open_project_engine));
+
   //vbox2_main
-
-
-  
   vbox2_main.append(vbox2_UX);
   vbox2_main.set_halign(Gtk::Align::END);
       //vbox2_UX
@@ -85,11 +78,6 @@ image("assets/images/violet.jpg")
   vbox2_UX.append(vbox2_buttons);
   
   
-
-
-      //vbo2_image
-
-
       //labels
   create_project_label.set_margin_top(20);
   create_project_label.set_margin_end(10);
@@ -129,6 +117,24 @@ image("assets/images/violet.jpg")
       //butto handle
   create_project_button.signal_clicked().connect(sigc::mem_fun(*this, &create_project::create_project_engine));
   cancel_project_button.signal_clicked().connect(sigc::mem_fun(*this, &create_project::cancel));
+  
+
+
+  //second page 
+  vbox3_main.append(scrolled_window);
+  scrolled_window.set_child(tree_view);
+  scrolled_window.set_policy(Gtk::PolicyType::AUTOMATIC, Gtk::PolicyType::AUTOMATIC);
+  scrolled_window.set_expand();
+
+  //Create the Tree model:
+  m_refTreeModel = Gtk::ListStore::create(m_Columns);
+  tree_view.set_model(m_refTreeModel);
+
+  tree_view.append_column("name",m_Columns.m_col_name);
+tree_view.signal_row_activated().connect(sigc::mem_fun(*this,
+              &create_project::on_treeview_row_activated) );
+
+
 
 }
 
@@ -171,6 +177,65 @@ void create_project::create_project_engine(){
 
   }else{
       std::cout<<"project already exists"<<std::endl;
+  }
+}
+
+void create_project::open_project_engine(Gtk::Widget* page, guint page_num) {
+    const char* home_dir = getenv("HOME");
+    std::string target_directory = std::string(home_dir) + "/proyects_FR";
+
+ if (page_num == 1) {
+        std::cout << "open project engine working well" << std::endl;
+        DIR* dir;
+        struct dirent *entry;
+        dir = opendir(target_directory.c_str());
+        if (dir == nullptr) {
+            std::cerr << "error opening the directory" << std::endl;
+            return;
+        } 
+
+
+        while ((entry = readdir(dir)) != nullptr) {
+            std::string nombre_archivo = entry->d_name;
+
+            if (nombre_archivo != "." && nombre_archivo != "..") {
+                auto row = *(m_refTreeModel->append());
+                row[m_Columns.m_col_name] = nombre_archivo;
+
+            }
+
+        }
+
+        closedir(dir); // Cerrar el directorio después de que el bucle haya terminado.
+    }
+}
+
+void create_project::on_treeview_row_activated(const Gtk::TreeModel::Path& path, Gtk::TreeViewColumn* column){
+    const auto iter = m_refTreeModel->get_iter(path);
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0){
+        std::cerr<<"no se pudo inicializar Sdl:"<<SDL_GetError()<<std::endl;
+    }
+
+  if(iter)
+  {
+    const auto row = *iter;
+    std::cout << "Row activated: ID=" << row[m_Columns.m_col_name] << ", Name="
+        << row[m_Columns.m_col_name] << std::endl;
+      
+        if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0){
+        std::cerr<<"no se pudo inicializar Sdl:"<<SDL_GetError()<<std::endl;
+      }
+        pid_t pid = fork();
+        
+        if(pid == 0 ){
+
+        Engine _engine;
+        _engine.inicicalizate();
+        _engine.shootdown();
+
+        }
+
+       set_visible(false);
   }
 }
 
